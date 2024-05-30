d56ff6a72ee893885476d01d712b0d7487d65a33
m0x0m0x <mx0mx@tuta.io>
2024-05-30 20:18:34 +0530

    li

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,8 +8,26 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
-
+/*
+https://forkify-api.herokuapp.com/v2
+Pakey - 6d693618-7b34-4bf1-ba2d-c5375c0770e5
+Example URL:https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=<insert your key>
+*/
 ///////////////////////////////////////
 
-console.log("heh");
+// Start fetcg reqyest
+
+// Fetch variable
+const myRec =
+  "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=6d693618-7b34-4bf1-ba2d-c5375c0770e5";
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(myRec);
+    const data = await res.json();
+    console.log(res, data);
+  } catch (error) {
+    alert(error);
+  }
+};
+showRecipe();
