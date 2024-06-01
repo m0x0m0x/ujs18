6dbcfa70b3438830e4e9c94b7315e186f2474b05
m0x0m0x <mx0mx@tuta.io>
2024-06-02 01:09:17 +0530

    fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -90,20 +90,7 @@ class RecipeView {
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
               ${this.#data.ingredients
-                .map((ing) => {
-                  return `
-                  <li class="recipe__ingredient">
-                  <svg class="recipe__icon">
-                    <use href="src/img/icons.svg#icon-check"></use>
-                  </svg>
-                  <div class="recipe__quantity">${ing.quantity}</div>
-                  <div class="recipe__description">
-                    <span class="recipe__unit">${ing.unit}</span>
-                    ${ing.description}
-                  </div>
-                </li>
-                `;
-                })
+                .map(this.#generateMarkupIngredient)
                 .join("")}
           </div>
 
@@ -128,6 +115,20 @@ class RecipeView {
             </a>
           </div>
     `;
+  }
+  #generateMarkupIngredient(ing) {
+    return `
+    <li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="src/img/icons.svg#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${ing.quantity}</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${ing.unit}</span>
+      ${ing.description}
+    </div>
+  </li>
+  `;
   }
 }
 
