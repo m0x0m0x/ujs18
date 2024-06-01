b5a8060cc6746dd5c290d2ecaad9d1422cca8380
m0x0m0x <mx0mx@tuta.io>
2024-06-01 20:18:13 +0530

    fix ingerediens

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,7 +60,9 @@ const showRecipe = async function () {
 
     const markup = `
       <figure class="recipe__fig">
-            <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+            <img src="${recipe.image}" alt="${
+      recipe.title
+    }" class="recipe__img" />
             <h1 class="recipe__title">
               <span>${recipe.title}</span>
             </h1>
@@ -71,14 +73,18 @@ const showRecipe = async function () {
               <svg class="recipe__info-icon">
                 <use href="src/img/icons.svg#icon-clock"></use>
               </svg>
-              <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+              <span class="recipe__info-data recipe__info-data--minutes">${
+                recipe.cookingTime
+              }</span>
               <span class="recipe__info-text">minutes</span>
             </div>
             <div class="recipe__info">
               <svg class="recipe__info-icon">
                 <use href="src/img/icons.svg#icon-users"></use>
               </svg>
-              <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+              <span class="recipe__info-data recipe__info-data--people">${
+                recipe.servings
+              }</span>
               <span class="recipe__info-text">servings</span>
 
               <div class="recipe__info-buttons">
@@ -110,35 +116,31 @@ const showRecipe = async function () {
           <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-              <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="src/img/icons.svg#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">1000</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">g</span>
-                  pasta
-                </div>
-              </li>
-
-              <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="src/img/icons.svg#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">0.5</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">cup</span>
-                  ricotta cheese
-                </div>
-              </li>
-            </ul>
+              ${recipe.ingredients
+                .map((ing) => {
+                  return `
+                  <li class="recipe__ingredient">
+                  <svg class="recipe__icon">
+                    <use href="src/img/icons.svg#icon-check"></use>
+                  </svg>
+                  <div class="recipe__quantity">${ing.quantity}</div>
+                  <div class="recipe__description">
+                    <span class="recipe__unit">${ing.unit}</span>
+                    ${ing.description}
+                  </div>
+                </li>
+                `;
+                })
+                .join("")}
           </div>
 
           <div class="recipe__directions">
             <h2 class="heading--2">How to cook it</h2>
             <p class="recipe__directions-text">
               This recipe was carefully designed and tested by
-              <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+              <span class="recipe__publisher">${
+                recipe.publisher
+              }</span>. Please check out
               directions at their website.
             </p>
             <a
@@ -153,6 +155,7 @@ const showRecipe = async function () {
             </a>
           </div>
     `;
+    recipeContainer.innerHTML = "";
     recipeContainer.insertAdjacentHTML("afterbegin", markup);
 
     console.log("Printing the destructured");
