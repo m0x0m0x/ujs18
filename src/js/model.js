4e82d9f40d0e87cc29493957e72174f000b1e1bf
m0x0m0x <mx0mx@tuta.io>
2024-06-04 10:13:52 +0530

    fixPageRoutingBug

--- src/js/model.js
+++ src/js/model.js
@@ -58,6 +58,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err}😡`);
     throw err;
